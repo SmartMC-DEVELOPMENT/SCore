c28ee93360631ab98f504958424fc8899ebcb970
Sergio <50914599+ImSergioh@users.noreply.github.com>
2024-02-25 16:22:24 +0100

    clean code servervariables

--- core-plugins/smartcore-spigot/src/main/java/us/smartmc/core/variables/ServerVariables.java
+++ core-plugins/smartcore-spigot/src/main/java/us/smartmc/core/variables/ServerVariables.java
@@ -3,20 +3,15 @@ package us.smartmc.core.variables;
 import me.imsergioh.pluginsapi.instance.VariableListener;
 import org.bukkit.entity.Player;
 import us.smartmc.core.SmartCore;
+import us.smartmc.core.util.VariableUtil;
 
 public class ServerVariables extends VariableListener<Player> {
 
     @Override
     public String parse(String message) {
-        if (message.equals("<serverId>")) {
-            return SmartCore.getServerID();
-        }
-        if (message.equals("<serverAlias>")) {
-            return SmartCore.getServerAlias();
-        }
-        if (message.equals("<serverNumber>")) {
-            return String.valueOf(SmartCore.getServerNumber());
-        }
+        message = VariableUtil.replace(message, "<serverId>", SmartCore.getServerID());
+        message = VariableUtil.replace(message, "<serverAlias>", SmartCore.getServerAlias());
+        message = VariableUtil.replace(message, "<serverNumber>", String.valueOf(SmartCore.getServerNumber()));
         return message;
     }
 

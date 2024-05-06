226a8d2b6e766458576b85be456785dcd176d824
Sergio <50914599+ImSergioh@users.noreply.github.com>
2024-05-06 19:12:54 +0200

    Update TagsHandler.java

--- smartcore-spigot/src/main/java/us/smartmc/core/handler/TagsHandler.java
+++ smartcore-spigot/src/main/java/us/smartmc/core/handler/TagsHandler.java
@@ -3,40 +3,23 @@ package us.smartmc.core.handler;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
-import com.comphenix.protocol.wrappers.EnumWrappers;
-import com.comphenix.protocol.wrappers.PlayerInfoData;
-import com.comphenix.protocol.wrappers.WrappedChatComponent;
-import com.comphenix.protocol.wrappers.WrappedGameProfile;
-import com.mojang.authlib.GameProfile;
 import me.imsergioh.pluginsapi.util.PaperChatUtil;
-import me.imsergioh.pluginsapi.util.SyncUtil;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.TextColor;
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
-import net.minecraft.network.chat.IChatBaseComponent;
 import org.bukkit.Bukkit;
-import org.bukkit.craftbukkit.v1_20_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 import us.smartmc.core.SmartCore;
 
-import java.lang.reflect.Field;
-import java.util.EnumSet;
-import java.util.List;
-import java.util.Objects;
 import java.util.OptionalInt;
 
 public class TagsHandler implements Listener {
@@ -106,18 +89,6 @@ public class TagsHandler implements Listener {
     }
 
     private String getUniqueTeamName(Player player) {
-        LuckPerms luckPerms = LuckPermsProvider.get();
-        String group = luckPerms.getUserManager().getUser(player.getUniqueId()).getPrimaryGroup();
-        OptionalInt entry = luckPerms.getGroupManager().getGroup(group).getWeight();
-
-        // Define un valor máximo de peso. Ajusta este valor según sea necesario.
-        long maxWeight = 99999999L;
-
-        // Calcula el valor inverso del peso para que un mayor peso resulte en un valor numérico más bajo.
-        long invertedWeight = maxWeight - entry.orElse(0);
-
-        // Combina el peso formateado con el nombre del grupo.
-
-        return String.format("%016d", invertedWeight);
+        return new StringBuilder(player.getUniqueId().toString()).substring(0, 12);
     }
 }

34f86dc70d5b0153703fa505535ed6f5a8308cda
Sergio <50914599+ImSergioh@users.noreply.github.com>
2024-05-09 13:51:57 +0200

    adsf

--- lobby-module/src/main/java/us/smartmc/lobbymodule/listener/TestAdminListeners.java
+++ lobby-module/src/main/java/us/smartmc/lobbymodule/listener/TestAdminListeners.java
@@ -1,6 +1,7 @@
 package us.smartmc.lobbymodule.listener;
 
 import me.imsergioh.pluginsapi.instance.item.ItemBuilder;
+import net.kyori.adventure.text.Component;
 import net.minecraft.network.protocol.game.PacketPlayOutEntity;
 import net.minecraft.network.protocol.game.PacketPlayOutEntityMetadata;
 import net.minecraft.network.protocol.game.PacketPlayOutSpawnEntity;
@@ -14,6 +15,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import us.smartmc.core.SmartCore;
 import us.smartmc.lobbymodule.util.TexturePackUtil;
@@ -47,14 +49,17 @@ public class TestAdminListeners extends AddonListener implements Listener {
             player.sendMessage("No sha1 could been readed from texturepack!");
             return;
         }
-        //player.setResourcePack(link, sha1);
 
-        Bukkit.getScheduler().runTaskLater(SmartCore.getPlugin(), () -> {
-            ItemStack customItem = ItemBuilder.of(Material.DIAMOND_SWORD).modelData(676).get();
-            ArmorStand armorStand = (ArmorStand) player.getLocation().getWorld().spawnEntity(player.getLocation(), EntityType.ARMOR_STAND);
+        player.setResourcePack(link, sha1);
 
-            armorStand.setHelmet(ItemBuilder.of(Material.DIAMOND_BLOCK).get());
-        }, 20);
+        ItemStack customItem = ItemBuilder.of(Material.DIAMOND_SWORD).modelData(676).get();
+        ArmorStand armorStand = (ArmorStand) player.getLocation().getWorld().spawnEntity(player.getLocation(), EntityType.ARMOR_STAND);
+        armorStand.setVisible(false);
+        armorStand.setGravity(false);
+        armorStand.setCanPickupItems(false);
+        armorStand.customName(Component.text("¡WELCOME!"));
+        armorStand.setCustomNameVisible(true);
+        armorStand.setItem(EquipmentSlot.HEAD, customItem);
     }
 
     public static void sendEntityMetadataPacket(Player player, ArmorStand armorStand) {

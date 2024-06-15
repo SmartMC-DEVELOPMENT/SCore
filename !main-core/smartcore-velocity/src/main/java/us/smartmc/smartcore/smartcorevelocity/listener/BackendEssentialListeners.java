fc1a79fc9ea846438def9661414e24d3c10f0177
Sergio <50914599+ImSergioh@users.noreply.github.com>
2024-06-15 11:59:05 +0200

    create smartcoreproxyplayer

--- !main-core/smartcore-velocity/src/main/java/us/smartmc/smartcore/smartcorevelocity/listener/BackendEssentialListeners.java
+++ !main-core/smartcore-velocity/src/main/java/us/smartmc/smartcore/smartcorevelocity/listener/BackendEssentialListeners.java
@@ -3,18 +3,23 @@ package us.smartmc.smartcore.smartcorevelocity.listener;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.DisconnectEvent;
 import com.velocitypowered.api.event.player.ServerConnectedEvent;
-import us.smartmc.backend.handler.ServicesManager;
-import us.smartmc.smartcore.smartcorevelocity.backend.service.PlayersService;
+import com.velocitypowered.api.proxy.Player;
+import us.smartmc.smartcore.smartcorevelocity.instance.player.SmartCoreProxyPlayer;
 
 public class BackendEssentialListeners {
 
     @Subscribe
     public void onConnect(ServerConnectedEvent event) {
-        ServicesManager.get(PlayersService.class).registerPlayerContext(event.getPlayer());
+        if (SmartCoreProxyPlayer.alreadyRegistered(event.getPlayer())) return;
+
+        SmartCoreProxyPlayer.get(event.getPlayer()).load();
     }
 
     @Subscribe
     public void onConnect(DisconnectEvent event) {
-        ServicesManager.get(PlayersService.class).unregisterPlayerContext(event.getPlayer());
+        Player player = event.getPlayer();
+        if (player.isActive()) return;
+
+        SmartCoreProxyPlayer.get(event.getPlayer()).unload();
     }
 }

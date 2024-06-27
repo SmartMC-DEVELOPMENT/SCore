27913eef1178676c160080037592b41a8d1686a1
Sergio <50914599+ImSergioh@users.noreply.github.com>
2024-06-27 21:15:20 +0200

    test regions

--- skyblock/skyblock-bukkit/src/main/java/us/smartmc/game/listener/TestListeners.java
+++ skyblock/skyblock-bukkit/src/main/java/us/smartmc/game/listener/TestListeners.java
@@ -9,6 +9,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Objects;
 
 public class TestListeners implements Listener {
 
@@ -20,24 +24,38 @@ public class TestListeners implements Listener {
         Player player = event.getPlayer();
         if (!player.getName().equals("ImSergioh")) return;
 
-        if (event.getMessage().contains("pos1")) {
-            p1 = player.getLocation();
-            if (!createRegion(player.getLocation().getWorld())) return;
-            player.sendMessage(PaperChatUtil.parse("&aCREADA REGIÓN TEST!"));
-        }
-
-        if (event.getMessage().contains("pos2")) {
-            p2 = player.getLocation();
-            if (!createRegion(player.getLocation().getWorld())) return;
-            player.sendMessage(PaperChatUtil.parse("&aCREADA REGIÓN TEST!"));
-        }
-
         if (event.getMessage().contains("fill")) {
             region.getBukkitLocationsList().forEachRemaining(location -> {
                 player.sendBlockChange(location, Material.DIAMOND_BLOCK.createBlockData());
             });
+            player.sendBlockChange(region.getBukkitPos1(), Material.REDSTONE_BLOCK.createBlockData());
+            player.sendBlockChange(region.getBukkitPos2(), Material.EMERALD_BLOCK.createBlockData());
+        }
+    }
+
+    @EventHandler
+    public void interactPositions(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        if (!player.getName().equals("ImSergioh")) return;
+        ItemStack item = player.getInventory().getItemInMainHand();
+        if (!item.getType().equals(Material.DIAMOND_AXE)) return;
+
+        if (!event.getAction().name().contains("BLOCK")) return;
+        boolean isPosition1 = event.getAction().name().contains("LEFT");
+
+        if (isPosition1) {
+            p1 = Objects.requireNonNull(event.getClickedBlock()).getLocation();
+            player.sendMessage(PaperChatUtil.parse("&aPOSITION 1 SET"));
+            event.setCancelled(true);
+
+        } else {
+            p2 = Objects.requireNonNull(event.getClickedBlock()).getLocation();
+            player.sendMessage(PaperChatUtil.parse("&aPOSITION 2 SET"));
+            event.setCancelled(true);
         }
 
+        if (!createRegion(player.getLocation().getWorld())) return;
+        player.sendMessage(PaperChatUtil.parse("&aCREADA REGIÓN TEST!"));
     }
 
     private boolean createRegion(World world) {

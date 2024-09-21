576ab76e8bacdfd26823e14ad32d6dc1f6d2fe90
ImSergioh <50914599+sergiomb04@users.noreply.github.com>
2024-09-21 18:30:04 +0200

    Update PlayerTeamsManager.java

--- games-core/src/main/java/us/smartmc/gamescore/manager/PlayerTeamsManager.java
+++ games-core/src/main/java/us/smartmc/gamescore/manager/PlayerTeamsManager.java
@@ -1,13 +1,14 @@
 package us.smartmc.gamescore.manager;
 
 import us.smartmc.gamescore.instance.manager.MapManager;
+import us.smartmc.gamescore.instance.player.PlayerTeam;
 
 import java.util.UUID;
 
-public class PlayerTeamsManager extends MapManager<UUID, GameTeamsManager> {
+public class PlayerTeamsManager extends MapManager<UUID, PlayerTeam> {
 
     @Override
-    public GameTeamsManager createValueByKey(UUID key) {
+    public PlayerTeam createValueByKey(UUID key) {
         return null;
     }
 }

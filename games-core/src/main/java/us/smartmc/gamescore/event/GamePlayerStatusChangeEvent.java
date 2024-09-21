55f273367229ce55950d34721d836eed999ebd51
ImSergioh <50914599+sergiomb04@users.noreply.github.com>
2024-09-21 18:05:41 +0200

    Create GamePlayerStatusChangeEvent.java

---
+++ games-core/src/main/java/us/smartmc/gamescore/event/GamePlayerStatusChangeEvent.java
@@ -0,0 +1,19 @@
+package us.smartmc.gamescore.event;
+
+import lombok.Getter;
+import org.bukkit.entity.Player;
+import us.smartmc.gamescore.instance.event.GameCorePlayerEvent;
+import us.smartmc.gamescore.instance.player.GameCorePlayer;
+import us.smartmc.gamescore.instance.player.PlayerStatus;
+
+@Getter
+public class GamePlayerStatusChangeEvent extends GameCorePlayerEvent {
+
+    private final PlayerStatus previousStatus, currentStatus;
+
+    public GamePlayerStatusChangeEvent(Player player, PlayerStatus previousStatus, PlayerStatus currentStatus) {
+        super(player);
+        this.previousStatus = previousStatus;
+        this.currentStatus = currentStatus;
+    }
+}

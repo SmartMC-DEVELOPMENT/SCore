d8253aceb3931968de90a792f98d7887b4bfb795
ImSergioh <50914599+sergiomb04@users.noreply.github.com>
2024-09-21 18:05:34 +0200

    split playerstatus from spectator

--- games-core/src/main/java/us/smartmc/gamescore/instance/player/PlayerStatus.java
+++ games-core/src/main/java/us/smartmc/gamescore/instance/player/PlayerStatus.java
@@ -2,6 +2,6 @@ package us.smartmc.gamescore.instance.player;
 
 public enum PlayerStatus {
 
-    LOBBY, PRE_GAME, GAME, POST_GAME, SPECTATOR
+    LOBBY, PRE_GAME, GAME, POST_GAME, SPECTATOR_DEATH, SPECTATOR_VIEWER
 
 }

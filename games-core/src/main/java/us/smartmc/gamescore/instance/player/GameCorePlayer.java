5995fb0cc4336676c2f425cc42e87bb196f9b375
ImSergioh <50914599+sergiomb04@users.noreply.github.com>
2024-09-21 18:06:05 +0200

    add cositas

--- games-core/src/main/java/us/smartmc/gamescore/instance/player/GameCorePlayer.java
+++ games-core/src/main/java/us/smartmc/gamescore/instance/player/GameCorePlayer.java
@@ -3,6 +3,8 @@ package us.smartmc.gamescore.instance.player;
 import lombok.Getter;
 import org.bukkit.entity.Player;
 import us.smartmc.gamescore.api.GamesCoreAPI;
+import us.smartmc.gamescore.event.GamePlayerStatusChangeEvent;
+import us.smartmc.gamescore.util.BukkitUtil;
 
 import java.util.UUID;
 
@@ -15,6 +17,26 @@ public class GameCorePlayer {
 
     public GameCorePlayer(UUID uuid) {
         this.uuid = uuid;
+
+        // Register to the playersManager
+        GamesCoreAPI.getPlayersManager().put(uuid, this);
+
+        // TODO: REMOVE THIS CONSUME PLAYER WHEN TESTED IN BUKKIT! :D
+        BukkitUtil.consumePlayer(uuid, player -> {
+            player.sendMessage("DEBUG: Se ha creado instancia GameCorePlayer!");
+        });
+    }
+
+    public void setStatus(PlayerStatus status) {
+        PlayerStatus previousStatus = this.status;
+        this.status = status;
+        BukkitUtil.getPlayer(uuid).ifPresent(bukkitPlayer -> {
+            BukkitUtil.callEvent(new GamePlayerStatusChangeEvent(bukkitPlayer, previousStatus, status));
+        });
+    }
+
+    public UUID getUUID() {
+        return uuid;
     }
 
     public static GameCorePlayer of(Player player) {

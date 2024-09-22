bf395eba1a6de48af8697c0d09ed28041320cea0
ImSergioh <50914599+sergiomb04@users.noreply.github.com>
2024-09-22 17:36:06 +0200

    Update CorePlayers.java

--- games-core/src/main/test/CorePlayers.java
+++ games-core/src/main/test/CorePlayers.java
@@ -11,21 +11,15 @@ public class CorePlayers {
     private static final TestStat stat = new TestStat();
 
     public static void main(String[] args) {
-        testStats();
-        GameCorePlayer gameCorePlayer = new GameCorePlayer(UUID.randomUUID());
-        for (int i = 0; i < 100; i++) {
-            stat.decrease(gameCorePlayer);
-        }
-        System.out.println("STAT = " + stat.getValue(gameCorePlayer));
-        System.out.println("DATA = " + gameCorePlayer.getStats().getDataDocument().toString());
-
         MongoDBConnection.mainConnection = new MongoDBConnection("localhost", 27017);
-
-        MongoDBData data = new MongoDBData("test_db", "test_collection", new Document("_id", gameCorePlayer.getUUID().toString()));
-        data.load();
-        System.out.println("DATA=" + data);
-        data.addAll(gameCorePlayer.getStats());
-        data.save();
+        testStats();
+        UUID id = UUID.fromString("c7332d8a-7481-4a49-a10f-b30fcd30098f");
+        GameCorePlayer gameCorePlayer = new GameCorePlayer(id);
+        gameCorePlayer.getStats().loadFromDatabase("test_db", "test_collection");
+        System.out.println("TEST STAT = " + stat.getValue(gameCorePlayer));
+        gameCorePlayer.getStats().loadFromDatabase("test_db", "new_collection");
+        System.out.println("DATA = " + gameCorePlayer.getStats().getDataDocument().toString());
+        gameCorePlayer.getStats().saveToDatabase(false, "test_db", "brand_collection");
     }
 
     private static void testStats() {

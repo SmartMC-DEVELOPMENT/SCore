5e122bb1bac25a3ed7c3710f938a060c013b8edd
ImSergioh <50914599+sergiomb04@users.noreply.github.com>
2024-09-22 18:05:03 +0200

    Update PlayerStatus.java

--- games-core/src/main/java/us/smartmc/gamescore/instance/player/PlayerStatus.java
+++ games-core/src/main/java/us/smartmc/gamescore/instance/player/PlayerStatus.java
@@ -2,6 +2,6 @@ package us.smartmc.gamescore.instance.player;
 
 public enum PlayerStatus {
 
-    LOBBY, PRE_GAME, GAME, POST_GAME, SPECTATOR_DEATH, SPECTATOR_VIEWER
+    LOBBY, PRE_GAME, IN_GAME, POST_GAME, SPECTATOR_DEATH, SPECTATOR_VIEWER
 
 }

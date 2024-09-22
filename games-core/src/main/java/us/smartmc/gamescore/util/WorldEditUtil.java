3c9bff47a8a05d2ce1cf5c1c304a0c7c7bcad4c9
ImSergioh <50914599+sergiomb04@users.noreply.github.com>
2024-09-22 16:10:37 +0200

    implement paste and load method

--- games-core/src/main/java/us/smartmc/gamescore/util/WorldEditUtil.java
+++ games-core/src/main/java/us/smartmc/gamescore/util/WorldEditUtil.java
@@ -1,27 +1,53 @@
 package us.smartmc.gamescore.util;
 
-import com.sk89q.worldedit.EditSession;
-import com.sk89q.worldedit.Vector;
-import com.sk89q.worldedit.WorldEdit;
-import com.sk89q.worldedit.WorldEditException;
+import com.sk89q.worldedit.*;
 import com.sk89q.worldedit.bukkit.BukkitUtil;
+import com.sk89q.worldedit.bukkit.adapter.BukkitImplAdapter;
 import com.sk89q.worldedit.extent.clipboard.BlockArrayClipboard;
+import com.sk89q.worldedit.extent.clipboard.Clipboard;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardReader;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardWriter;
 import com.sk89q.worldedit.function.operation.ForwardExtentCopy;
+import com.sk89q.worldedit.function.operation.Operation;
 import com.sk89q.worldedit.function.operation.Operations;
 import com.sk89q.worldedit.regions.CuboidRegion;
 import com.sk89q.worldedit.regions.Region;
+import com.sk89q.worldedit.session.ClipboardHolder;
+import com.sk89q.worldedit.session.PasteBuilder;
+import com.sk89q.worldedit.world.registry.WorldData;
 import org.bukkit.Location;
 import org.bukkit.World;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.Objects;
 import java.util.function.Consumer;
 
 public class WorldEditUtil {
+
+    public static void paste(Location location, File file, Consumer<PasteBuilder> optionsConsumer) throws IOException, WorldEditException {
+        Clipboard clipboard = loadFromFile(location.getWorld(), file);
+        if (clipboard == null) {
+            throw new IOException("Failed to load clipboard");
+        }
+        com.sk89q.worldedit.world.World weWorld = BukkitUtil.getLocalWorld(location.getWorld());
+        EditSession editSession = WorldEdit.getInstance().getEditSessionFactory().getEditSession(weWorld, -1);
+        PasteBuilder holder = new ClipboardHolder(clipboard, weWorld.getWorldData())
+                .createPaste(editSession, weWorld.getWorldData())
+                .to(new Vector(location.getBlockX(), location.getBlockY(), location.getBlockZ()))
+                .ignoreAirBlocks(false);
+        optionsConsumer.accept(holder);
+        Operation operation = holder.build();
+        Operations.complete(operation);
+    }
+
+    private static Clipboard loadFromFile(World world, File file) throws IOException {
+        WorldData worldData = BukkitUtil.getLocalWorld(world).getWorldData();
+        ClipboardFormat format = ClipboardFormat.findByFile(file);
+        if (format == null) return null;
+        ClipboardReader reader = format.getReader(new FileInputStream(file));
+        return reader.read(worldData);
+    }
 
     public static BlockArrayClipboard getClipboard(Location corner1, Location corner2) {
         com.sk89q.worldedit.world.World weWorld = BukkitUtil.getLocalWorld(corner1.getWorld());

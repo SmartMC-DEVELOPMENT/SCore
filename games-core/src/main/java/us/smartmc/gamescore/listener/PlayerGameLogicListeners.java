027af987409da136346a2f2cfef85a4b0516db6e
ImSergioh <50914599+sergiomb04@users.noreply.github.com>
2024-09-22 18:30:16 +0200

    Update PlayerGameLogicListeners.java

--- games-core/src/main/java/us/smartmc/gamescore/listener/PlayerGameLogicListeners.java
+++ games-core/src/main/java/us/smartmc/gamescore/listener/PlayerGameLogicListeners.java
@@ -1,14 +1,43 @@
 package us.smartmc.gamescore.listener;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import us.smartmc.gamescore.api.GamesCoreAPI;
 import us.smartmc.gamescore.event.player.GamePlayerQuitEvent;
+import us.smartmc.gamescore.instance.game.Game;
+import us.smartmc.gamescore.instance.player.GameCorePlayer;
+import us.smartmc.gamescore.instance.player.PlayerStatus;
 
 public class PlayerGameLogicListeners implements Listener {
 
     @EventHandler
     public void onPlayerQuit(GamePlayerQuitEvent event) {
         GamesCoreAPI.getGamesManager().unregisterPlayer(event.getBukkitPlayer());
+    }
+
+    @EventHandler
+    public void cancelDamageIfNotInGame(EntityDamageEvent event) {
+        if (event.getEntity() instanceof Player player) {
+            GameCorePlayer gamePlayer = GameCorePlayer.of(player);
+            if (gamePlayer == null) return;
+            if (gamePlayer.getStatus().equals(PlayerStatus.IN_GAME)) {
+                event.setCancelled(false);
+                return;
+            }
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent event) {
+        Player player = event.getEntity();
+        GameCorePlayer gamePlayer = GameCorePlayer.of(player);
+        Game game = gamePlayer.getCurrentGame();
+        if (game == null) return;
+        game.killPlayer(gamePlayer);
     }
 }
